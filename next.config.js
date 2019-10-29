a706027d934677b10daa1460c008c3a72695c156
Guillermo Magdaleno <gmpco51@gmail.com>
2019-10-28 21:10:57 -0700

    10/28 Static Site

--- next.config.js
+++ next.config.js
@@ -2,18 +2,23 @@ const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS(withSass({
-   webpack (config, options) {
-       config.module.rules.push({
-           test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-           use: {
-               loader: 'url-loader',
-               options: {
-                   limit: 100000
-               }
-           }
-       });
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000
+                }
+            }
+        });
 
-       return config;
-   },
-   assetPrefix: process.env.NODE_ENV === 'production' ? '/giemper-site' : ''
+        return config;
+    },
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+    exportPathMap: function () {
+        return {
+            '/': { page: '/' }
+        };
+    }
 }));

b792e419f3cd0008f87164c4bfe2d3bf866e15d9
Guillermo Magdaleno <gmpco51@gmail.com>
2019-11-08 00:07:14 -0800

    11/8 Added Projects

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -1,59 +1,70 @@
 import '../styles/ProjectItem.scss';
 import checkItem from '../tools/checkItem';
 
-const Browser = () => (
-    <>
-    </>
-);
-
-const Phone = (props) => (
-    <video className="project-preview" autoPlay loop muted>
-        <source src={props.main} type="video/webm" />
-        <source src={props.fallback} type="video/mp4" />
-    </video>
-);
+const Preview = (props) => {
+    const type = (props.type === 'Phone') ? "phone-preview" : "browser-preview";
+    return (
+        <video className={type} autoPlay loop muted>
+            <source src={props.main} type="video/webm" />
+            <source src={props.fallback} type="video/mp4" />
+        </video>
+    );
+}
 
 const ProjectItem = (props) => {
     const lang = props.languageNum;
-    const availableAt = ["Available at:", "Displonible en:"]
-    const projectType = (props.type === 'Phone')
-                    ? <Phone main={props.sourceMain} fallback={props.sourceFallback} />
-                    : <Browser />;
-    const projectAvailability = (props.availability !== undefined)
-                    ? <h3>{checkItem(availableAt, lang)}</h3>
-                    : <></>;
-    const order = (props.index % 2 == 0)
-                    ? 'order-last'
-                    : 'order-first';
-    const desc = checkItem(props.description, lang);
+    const descriptions = checkItem(props.description, lang);
+    const availableAt = ["Available at", "Displonible en"];
+    const order = (props.index % 2 == 0) ? 'order-last' : 'order-first';
+    const textClasses = (props.type === 'Phone' || props.type === 'Browser') 
+                        ? "col-lg-6 col-md-6 col-sm-12 project-pad"
+                        : "col-12 project-pad";
+    const previewClasses = (props.type === 'Phone' || props.type === 'Browser')  
+                        ? "col-lg-6 col-md-6 col-sm-12 project-pad " + order
+                        : "";
+    const textCenter = (props.type === 'Phone') ? "center" : "";
+    const previewCenter = (props.type === 'Phone') ? "" : "center";
+
 
     return (
         <div className="project-item">
             <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-12 project-pad">
-                    <div className="project-text">
+                <div className={textClasses}>
+                    <div className={`project-text ${textCenter}`}>
                         <h1>{props.title}</h1>
-                        { desc !== undefined &&
-                            desc.map((item, index) => (
+                        { 
+                            descriptions !== undefined &&
+                            descriptions.map((item, index) => ( 
                                 <p key={index}>{item}</p>
                             ))
                         }
-                        {projectAvailability}
-                        <div>
-                            { props.availability !== undefined &&
-                                props.availability.map((item, index) => (
-                                    <a key={index} 
-                                        href={item.link} 
-                                        target="_bind"
-                                        className="project-link">{item.title}</a>
-                                ))
-                            }
-                        </div>
+                        { 
+                            props.availability !== undefined &&
+                            <>
+                                <h3>{checkItem(availableAt, lang)}</h3>
+                                <div>
+                                    {
+                                        props.availability.map((item, index) => (
+                                            <a key={index} 
+                                                href={item.link} 
+                                                target="_bind"
+                                                className="project-link">{item.title}</a>
+                                        ))
+                                    }
+                                </div>
+                            </>
+                        }
+
                     </div>
                 </div>
-                <div className={`col-lg-6 col-md-6 col-sm-12 project-pad ${order}`}>
-                    {projectType}
-                </div>
+                {
+                    props.sourceMain !== undefined &&
+                    <div className={previewClasses}>
+                        <div className={`project-preview ${previewCenter}`}>
+                            <Preview type={props.type} main={props.sourceMain} fallback={props.sourceFallback} />
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     )
--- values/VALUES_PROJECTS.js
+++ values/VALUES_PROJECTS.js
@@ -5,8 +5,16 @@ const VALUES_PROJECTS = [
         sourceMain: "videos/EcoCarWash.webm",
         sourceFallback: "videos/EcoCarWash.mp4",
         description: [
-            ["Project done for a car wash company in Baja California, Mexico.", "The app tracks follows, registers, and reports the cleaning life cycle in which a car might go through: the car entering the car wash, an employee being assigned for cleaning, and completition of work."],
-            ["Proyecto hecho para un compañia de auto-lavados en Baja California, México.", "La aplicacion sigue, registra, y reportea el ciclo de vida del lavado por el que puede pasar cada automóvil: desde que ingresa al auto-lavado, cuando se asigna un limpeador, y cuando el trabajo fue terminado."]
+            [
+                "Project done for a car wash company in Baja California, Mexico.", 
+                "The app tracks follows, registers, and reports the cleaning life cycle in which a car might go through: the car entering the car wash, an employee being assigned for cleaning, and completition of work.",
+                "The purpose was not only to report assistance and performance, but also to balance the workload by assigning the next jobs automatically among employees."
+            ],
+            [
+                "Proyecto hecho para un compañia de auto-lavados en Baja California, México.", 
+                "La aplicacion sigue, registra, y reportea el ciclo de vida del lavado por el que puede pasar cada automóvil: desde que ingresa al auto-lavado, cuando se asigna un limpeador, y cuando el trabajo fue terminado.",
+                "El proposito era no solo reportar la asistencia y desempeño, si no tambien balancear la carga de trabajo al asignar automaticamente lo siguentes trabajos entre empleados."
+            ]
         ],
         availability: [
             { title: "Play Store", link: "https://play.google.com/store/apps/details?id=com.giemper.ecocarwash" },
@@ -15,12 +23,18 @@ const VALUES_PROJECTS = [
     },
     {
         title: "Colour App",
-        type: 'Phone',
+        type: "Phone",
         sourceMain: "videos/Colour.webm",
         sourceFallback: "videos/Colour.mp4",
         description: [
-            ["This project was done a few years ago as practice for Android Development", "The goal of Colour is to be a minimalist color picker for your phone for when you're in need of picking a quick color reference for a project."],
-            ["Este projecto fue hecho hace un par de años como practica en desarrollo para Android", "La meta de Colour es ser un seleccionador de colores minimalista, para cuando uno este en necesidad de escoger una referencia de color rapida para un proyecto."]
+            [
+                "This project was done a few years ago as practice for Android Development", 
+                "The goal of Colour is to be a minimalist color picker for your phone for when you're in need of picking a quick color reference for a project."
+            ],
+            [
+                "Este projecto fue hecho hace un par de años como practica en desarrollo para Android", 
+                "La meta de Colour es ser un seleccionador de colores minimalista, para cuando uno este en necesidad de escoger una referencia de color rapida para un proyecto."
+            ]
         ],
         availability: [
             { title: "Github", link: "https://github.com/Giemper/Colour" }
@@ -28,16 +42,68 @@ const VALUES_PROJECTS = [
     },
     {
         title: "Giemper.com",
-        type: 'Browser',
+        type: "Browser",
         sourceMain: "videos/Giempercom.webm",
         sourceFallback: "videos/Giempercom.mp4",
         description: [
-            [""],
-            [""]
+            [
+                "Look! It's the site you're on right now!", 
+                "The site was done using React + NextJS, Bootstrap Grids, Google Fonts, and a few Font Awesome icons.", 
+                "Fun Fact: The domain is called Giemper.com because of my full name initials.", 
+                "Guillermo Magdaleno Perez. GMP. Gee-Em-Pe. Giemper."
+            ],
+            [
+                "¡Mira, es el sitio en el que estas ahorita mismo!", 
+                "El sitio fue hecho usando React + NextJS, Bootstrap Grids, Google Fonts, y unos cuantos iconos de Font Awesome.", 
+                "Dato Curioso: El dominio se llama Giemper.com gracias a las iniciales de mi nombre completo.", 
+                "Guillermo Magdaleno Perez. GMP. Gee-Em-Pe. Giemper."]
         ],
         availability: [
             { title: "Giemper.com", link: "#nav_home" },
-            { title: "Github", link: "http://github.com/Giemper/giemper-site" }
+            { title: "Github", link: "https://github.com/Giemper/giemper-site" }
+        ]
+    },
+    {
+        title: "Posada 2016",
+        type: "Browser",
+        sourceMain: "videos/Giempercom.webm",
+        sourceFallback: "videos/Giempercom.mp4",
+        description: [
+            [
+                "Since high school, every year around Christmas eve a my friends and I organized a Secret Santa gift exchange amoung ourselves. While we all get along, there are sometimes when you just don't want to get matched with someone specific.",
+                "Starting on 2014, I came up with the idea of doing a site to help us with that conundrum. The site not only did the matching without ruining the surprise for anyone, but filtered out any 'incompatible' matches.",
+                "Every participant was able to create a profile, answer some questions about themselves to help their Santa, read the preferences and send annonymous messages to who they were supposed to give a gift to.",
+                "While we keep meeting for our annual Christmas party, sadly 2016 was the last year of our Secret Santa exchange. Maybe I should give this project another go soon!"
+            ],
+            [
+                "Desde que estabamos en preparatoria, cada año cerca de Navidad mis amigos y yo nos organizamos para un intercambio navideño 'Santa Secreto' entre nosotros. Aunque todos nos llevamos muy bien, hay ocaciones en los cuales uno no quiere ser asignado con alguien especifico.",
+                "Empezando en 2014, tuve la idea de diseñar un sitio que nos ayude con este problema. El sitio no solo permite hacer la asignación del intercambio sin arruinar la sorpresa para nadie, si no tambien filtra cualquier asignación 'incompatible'.",
+                "Cada participante puede crear su perfil, responder algunas preguntas sobre si mismos para ayudar a su Santa, y enviar mensajes anonimos y leer las preferencias de la persona a quien le tienen que dar un regalo.",
+                "Aunque seguimos haciendo nuestra fiesta Navideña Anual, tristemente 2016 fue el ultimo año del intercambio de regalos. Quizas deberia pronto darle otra oportunidad a este proyecto."
+            ]
+        ],
+        availability: [
+            { title: "Github", link: "https://github.com/Giemper/Posada2016" }
+        ]
+    },
+    {
+        title: "HRPoint",
+        type: "None",
+        description: [
+            [
+                "HRPoint is the intranet webpage for Collins Aerospace Mexicali.",
+                "Employees of Collins Aerospace can get into the site to check the latest news and happenings in our facilities, check a roadmap of their career development in the company, check employee benefits, find and fill request forms for things like Paid Time Off, check the weekly cafeteria menu, calendars, and inquiries with Human Resources, Finance, and Health & Security departments.",
+                "Some features and sections are locked depending on the management level, as they might have information and forms limited for exclusive use of supervisors, like requesting monetary rewards for employees based on performance.",
+                "The newsletter and content of the site is constantly updated by the Communications department by using a blog-like interface in which they can choose how the information is going to be deployed. It could be just a new post, it could be an announcent with a header image in the homepage, or a site-wide email.",
+                "HRPoint is available only in the Collins Aerospace internal network."
+            ],
+            [
+                "HRPoint es la pagina intranet de Collins Aerospace Mexicali.",
+                "Empleados de Collins Aerospace puenden entrar al sitio para checar las noticias y eventos más recientes en nuestras instalaciones, ver opciones para desarollo profesional, consultar los beneficios como empleado, encontrar y llenar formatos para cosas como permisos con gose, ver el menú semanal de la cafetería, calendarios, y consultas con los departamentos de Recursos Humanos, Finanzas, y Seguridad.",
+                "Algunas funciones estan restrigidas dependiendo del nivel de manejo, ya que pueden contar con información y formatos limitadas para uso exclusivo de supervisores, como solitar recomentazas economicas para empleados basado en desempeño.",
+                "El boletin informativo y contenido del sitio es constantemente actualizado por el departamento de Comunicación usando una interface tipo blog, en el cual pueden escoger como la información va a ser distribuida. Puede ser solamente un nueva publicación, puede ser un anuncio visual en la página de inicio, o puede ser un correo electronico para toda el sitio.",
+                "HRPoint esta disponible solamente en la red interna de Collins Aerospace."
+            ]
         ]
     }
 ];
